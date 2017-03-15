db90b8d11f74baa2c1f349db6e7cfc4510937487
Micael Persson <micael.c.persson@gmail.com>
2017-03-15 12:40:13 +0100

    Working on a seed file for the database.

--- server/api/routes.js
+++ server/api/routes.js
@@ -3,6 +3,9 @@
 const express = require('express')
 const router = express.Router()
 
+// TODO: 4. Setup API endpoint and serve current seeded data.
+// TODO: 5. Setup API endpoint for user to trigger data collection from github.
+
 router.get('/test', (req, res, next) => {
   res.json({ 'Text': 'Imported route file works.' })
 })
---
+++ server/config/seed.js
@@ -0,0 +1,5 @@
+'use strict'
+
+// TODO: 1. Create 6 repos.
+// TODO: 2. Create 3 Organization.
+// TODO: 3. Save to database.
